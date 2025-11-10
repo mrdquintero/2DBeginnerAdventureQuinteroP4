b47f3fd8ff3ee6ef0352c48f3313b47513108f86
David Quintero <dquintero@dusd.net>
2025-11-10 11:58:24 -0800

    read keyboard input to control
    Completed tutorial. Start Input Action for Player next class

--- 2DBeginnerAdventureP4Quintero/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP4Quintero/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,8 +14,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+
+        if(Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {  
+            horizontal = 1.0f;
+
+        }
+        Debug.Log(horizontal);
+        
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if(Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.001f;
+        position.x = position.x + 0.01f * horizontal;
+        position.y = position.y + 0.01f * vertical;
         transform.position = position;
 
         
